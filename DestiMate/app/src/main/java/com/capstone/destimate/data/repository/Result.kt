31b0e0bf66a4c8927d3d58b31260523dc6ac7cd7
dadenkasandi08 <dadenkasandi43@mhs.unram.ac.id>
2023-12-22 12:42:48 +0800

    delete wrong app

--- DestiMate/app/src/main/java/com/capstone/destimate/data/repository/Result.kt
+++
@@ -1,7 +0,0 @@
-package com.capstone.destimate.data.repository
-
-sealed class Result<out R> private constructor() {
-    data class Success<out T>(val data: T) : Result<T>()
-    data class Error(val error: String) : Result<Nothing>()
-    data object Loading : Result<Nothing>()
-}
